0e8d81c249494661f8faddef3d0b22782d3b3948
Lukasz Wojdanowicz <lukasz.wojdanowicz.ext@nokia.com>
2016-11-14 11:59:32 +0100

    SFINAE

--- 50-sfinae/implementation.h
+++ 50-sfinae/implementation.h
@@ -1,8 +1,28 @@
 #pragma once
 
-template<typename T>
-struct has_type_typedef;
 
 template<typename T>
-struct has_member_variable;
+struct has_type_typedef{
+
+	template <typename U, typename = typename U::type>
+	static void test(int);
+//	static auto test(int) -> decltype(std::declval<typename U::type>(), void());
+	template <typename U>
+	static char test(long);
+public:
+	static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+	
+};
+
+template<typename T>
+struct has_member_variable{
+
+	template <typename U>
+	static auto test(int) -> decltype(std::declval<U>().member, void());
+	template <typename U>	
+	static char test(long);
+
+public:
+	static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+};
 
